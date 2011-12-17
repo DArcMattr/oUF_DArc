fa91c8c2ef1333cfee0913b115e8bd788aba2ba4
David Arceneaux <david@davidthemachine.org>
2011-12-16 18:22:34 -0800

    remake whitespace into my own preferred format name coloring is class-based, regardless of unit my namebar element is working at the point of not being broken, I have not tested the full range of options, but I know that unit reaction coloring isn't working as I'd hoped

--- tags.lua
+++ tags.lua
@@ -1,40 +1,40 @@
+oUF.TagEvents["DArc:unittype"] = "UNIT_NAME_UPDATE"
 oUF.Tags["DArc:unittype"] = function(unit)
   return UnitReaction(unit, "player")
 end
-oUF.TagEvents["DArc:unittype"] = "UNIT_NAME_UPDATE"
 
 -- from oUF_Neav
-oUF.TagEvents['name'] = 'UNIT_NAME_UPDATE'
-oUF.Tags['name'] = function(unit)
-    local r, g, b
-    local colorA, colorB
-    local unitName, unitRealm = UnitName(unit)
-    local _, class = UnitClass(unit)
+oUF.TagEvents['DArc:name'] = 'UNIT_NAME_UPDATE'
+oUF.Tags['DArc:name'] = function(unit)
+  local r, g, b
+  local colorA, colorB
+  local unitName, unitRealm = UnitName(unit)
+  local _, class = UnitClass(unit)
 
-    if (unitRealm) and (unitRealm ~= '') then
-        unitName = unitName..' (*)'
+  if (unitRealm) and (unitRealm ~= '') then
+    unitName = unitName..' (*)'
+  end
+
+  for i = 1, 4 do
+    if (unit == 'party'..i) then
+      colorA = oUF.colors.class[class]
     end
+  end
 
-    for i = 1, 4 do
-        if (unit == 'party'..i) then
-            colorA = oUF.colors.class[class]
-        end
-    end
+  --if (unit == 'player' or not UnitIsFriend('player', unit) and UnitIsPlayer(unit) and UnitClass(unit)) then
+    colorA = oUF.colors.class[class]
+  --elseif (unit == 'targettarget' and UnitIsPlayer(unit) and UnitClass(unit)) then
+  --  colorA = oUF.colors.class[class]
+  --else
+  --  colorB = {1, 1, 1}
+  --end
 
-    if (unit == 'player' or not UnitIsFriend('player', unit) and UnitIsPlayer(unit) and UnitClass(unit)) then
-		colorA = oUF.colors.class[class]
-	elseif (unit == 'targettarget' and UnitIsPlayer(unit) and UnitClass(unit)) then
-		colorA = oUF.colors.class[class]
-	else
-		colorB = {1, 1, 1}
-	end
+  if (colorA) then
+    r, g, b = colorA[1], colorA[2], colorA[3]
+  elseif (colorB) then
+    r, g, b = colorB[1], colorB[2], colorB[3]
+  end
 
-	if (colorA) then
-		r, g, b = colorA[1], colorA[2], colorA[3]
-	elseif (colorB) then
-		r, g, b = colorB[1], colorB[2], colorB[3]
-	end
-    
   --if (unitRealm) and (unitRealm ~= '') then
     return format('|cff%02x%02x%02x%s|r', r*255, g*255, b*255, unitName)     -- no abbrev
   --else
@@ -43,12 +43,13 @@ oUF.Tags['name'] = function(unit)
   --return format('|cff%02x%02x%02x%s|r', r*255, g*255, b*255, string.gsub(unitName, '%s?(.[\128-\191]*)%S+%s', '%1. '))   -- abbrev all words except the last
 end
 
-oUF.Tags['level'] = function(unit)
+oUF.TagEvents["DArc:level"] = "UNIT_LEVEL PLAYER_TARGET_CHANGED PLAYER_LEVEL_UP"
+oUF.Tags['DArc:level'] = function(unit)
     local r, g, b
     local level = UnitLevel(unit)
     local colorL = GetQuestDifficultyColor(level)
 
-    if (level < 0) then 
+    if (level < 0) then
         r, g, b = 1, 0, 0
         level = '??'
     elseif (level == 0) then
